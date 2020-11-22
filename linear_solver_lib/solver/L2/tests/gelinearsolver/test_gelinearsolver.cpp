539c96416d7e0434e0775cc012e1ff796a6efaa8
jacksoncd <jackson.cooper-driver@wadham.ox.ac.uk>
2020-11-22 09:57:59 +0000

    Update Makefile

--- linear_solver_lib/solver/L2/tests/gelinearsolver/test_gelinearsolver.cpp
+++ linear_solver_lib/solver/L2/tests/gelinearsolver/test_gelinearsolver.cpp
@@ -106,8 +106,8 @@ int main(int argc, const char* argv[]) {
     int NB = 2;
     
     
-    int first_row = 4;
-    int final_row = 5;
+    int first_row = 500;
+    int final_row = 700;
     int max_row = 0;
     
     int execution_times[final_row - first_row];
@@ -156,13 +156,13 @@ int main(int argc, const char* argv[]) {
     for (int i = 0; i < dataAM; ++i) {
         for (int j = 0; j < dataAN; ++j) {
             dataA[i * dataAN + j] = dataE[i][j];
-            printf("Data A Row %d Column %d : %f \n",i,j,dataA[i * dataAN + j]);
+            //printf("Data A Row %d Column %d : %f \n",i,j,dataA[i * dataAN + j]);
         }
     }
     for (int i = 0; i < dataAM; ++i) {
         for (int j = 0; j < NB; ++j) {
-            dataB[i * NB + j] = i*1000;
-            printf("Data B Row %d Column %d : %f \n",i,j,dataB[i * NB + j]);
+            dataB[i * NB + j] = i;
+            //printf("Data B Row %d Column %d : %f \n",i,j,dataB[i * NB + j]);
         }
     } 
     
@@ -290,7 +290,7 @@ int main(int argc, const char* argv[]) {
 
     // Calculate err between dataA and dataC
     for (int i = 0; i < inoutB_size; i++){
-        printf("Data x Row %d : %f \n",i,dataB[i]);
+        //printf("Data x Row %d : %f \n",i,dataB[i]);
     }
     
     double errA = 0;
