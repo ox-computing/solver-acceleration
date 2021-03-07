75d4fd68aa4bd0a3035d92ba2ecd6172785d283f
jacksoncd <jackson.cooper-driver@wadham.ox.ac.uk>
2021-03-07 18:07:08 +0000

    Update functions

--- linear_solver_lib/solver/L2/include/hw/LinearSolver/gelinearsolver.hpp
+++ linear_solver_lib/solver/L2/include/hw/LinearSolver/gelinearsolver.hpp
@@ -220,8 +220,9 @@ void gelinearsolver(int num_nonzeros, int new_matrix, int n, int num_rhs, int* i
       //#pragma HLS resource variable = matA2 core = XPM_MEMORY uram
 
       
-      #pragma HLS bind_storage variable = matA type = ram_t2p impl = uram
-      #pragma HLS bind_storage variable = matB type = ram_t2p impl = uram
+      //#pragma HLS bind_storage variable = matA1 type = ram_t2p impl = uram
+      //#pragma HLS bind_storage variable = matA2 type = ram_t2p impl = uram
+      //#pragma HLS bind_storage variable = matB type = ram_t2p impl = uram
       
       
          for (int j = 0; j < num_rhs; j++) {
