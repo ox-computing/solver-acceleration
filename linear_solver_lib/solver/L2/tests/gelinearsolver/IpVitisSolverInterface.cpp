eed1c3a60688ba2e7f4b217d32a40cccd9785ebe
jacksoncd <jackson.cooper-driver@wadham.ox.ac.uk>
2021-04-02 17:54:23 +0100

    Update

--- linear_solver_lib/solver/L2/tests/gelinearsolver/IpVitisSolverInterface.cpp
+++ linear_solver_lib/solver/L2/tests/gelinearsolver/IpVitisSolverInterface.cpp
@@ -369,7 +369,7 @@ namespace Ipopt
           Storing timing data to txt file
           *************/
           
-          /*int array_setup = diff(&tinit_array,&tstart);
+          int array_setup = diff(&tinit_array,&tstart);
           int trans1 = diff(&ttrans1,&tinit_array);
           int launch = diff(&tlaunch,&ttrans1);
           int trans2 = diff(&ttrans2,&tlaunch);
@@ -410,7 +410,7 @@ namespace Ipopt
           fprintf(fp,"Second transfer : %d \n", trans2);
           fprintf(fp,"Post : %d \n", post);
           
-          fclose(fp);*/
+          fclose(fp);
           
           if(solver_singular)
           {
