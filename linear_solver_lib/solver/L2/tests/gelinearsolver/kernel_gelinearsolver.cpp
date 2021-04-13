7a73460a8db81ca9bf9c2334072e6f075aed0921
jacksoncd <jackson.cooper-driver@wadham.ox.ac.uk>
2021-04-13 13:45:56 +0100

    Implementing qdldl

--- linear_solver_lib/solver/L2/tests/gelinearsolver/kernel_gelinearsolver.cpp
+++ linear_solver_lib/solver/L2/tests/gelinearsolver/kernel_gelinearsolver.cpp
@@ -14,12 +14,15 @@
  * limitations under the License.
  */
 
-#include "xf_solver_L2.hpp"
+//#include "xf_solver_L2.hpp"
+
+#include "qdldl.hpp"
+
 //#define NCU 36
-#define MAXN 998
+#define MAXN 10
 
 
-extern "C" void kernel_gelinearsolver_0(int debug_mode, int num_nonzeros, int new_matrix, int n, int num_rhs, int* A_rows, int* A_cols, double* A_vals, double* dataB) {
+/*extern "C" void kernel_gelinearsolver_0(int debug_mode, int num_nonzeros, int new_matrix, int n, int num_rhs, int* A_rows, int* A_cols, double* A_vals, double* dataB) {
 #pragma HLS INTERFACE m_axi port = A_rows bundle = gmem0 offset = slave max max_read_burst_length = 128
 #pragma HLS INTERFACE m_axi port = A_cols bundle = gmem0 offset = slave max_read_burst_length = 128
 #pragma HLS INTERFACE m_axi port = A_vals bundle = gmem0 offset = slave max_read_burst_length = 128
@@ -33,12 +36,77 @@ extern "C" void kernel_gelinearsolver_0(int debug_mode, int num_nonzeros, int ne
 #pragma HLS INTERFACE s_axilite port = A_cols bundle = control
 #pragma HLS INTERFACE s_axilite port = A_vals bundle = control
 #pragma HLS INTERFACE s_axilite port = dataB bundle = control
+#pragma HLS INTERFACE s_axilite port = return bundle = control*/
+
+extern "C" void kernel_gelinearsolver_0(QDLDL_int An, QDLDL_int* Ap, QDLDL_int* Ai, QDLDL_float* Ax, QDLDL_float* b) {
+#pragma HLS INTERFACE m_axi port = Ap bundle = gmem0 offset = slave max max_read_burst_length = 128
+#pragma HLS INTERFACE m_axi port = Ai bundle = gmem0 offset = slave max_read_burst_length = 128
+#pragma HLS INTERFACE m_axi port = Ax bundle = gmem0 offset = slave max_read_burst_length = 128
+#pragma HLS INTERFACE m_axi port = b bundle = gmem1 offset = slave max_read_burst_length = 128 max_write_burst_length = 128
+
+#pragma HLS INTERFACE s_axilite port = An bundle = control
+#pragma HLS INTERFACE s_axilite port = Ap bundle = control
+#pragma HLS INTERFACE s_axilite port = Ai bundle = control
+#pragma HLS INTERFACE s_axilite port = Ax bundle = control
+#pragma HLS INTERFACE s_axilite port = b bundle = control
 #pragma HLS INTERFACE s_axilite port = return bundle = control
 
-    
-    //int debug_mode = 0;
-  
-       // General linear solver
-       xf::solver::gelinearsolver<double, MAXN, NCU_IN>(debug_mode, num_nonzeros, new_matrix, n, num_rhs, A_rows, A_cols, A_vals, dataB);
+// Allocate storage values
+QDLDL_int Ln = An;
+
+QDLDL_int Lp[MAXN + 1];
+QDLDL_int etree[MAXN];
+QDLDL_int Lnz[MAXN];
+
+QDLDL_float D[MAXN];
+QDLDL_float Dinv[MAXN];
+
+QDLDL_int iwork[3*MAXN];
+QDLDL_bool bwork[MAXN];
+QDLDL_float fwork[MAXN];
+
+// Elimination tree calculation
+
+QDLDL_int sumLnz = QDLDL_etree(An,Ap,Ai,iwork,Lnz,etree);
+
+
+
+// LDL Factorisation
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     
 }
