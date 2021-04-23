00ed4b51149d4526e0ead7e09ab1b66fab52fb1f
Jackson Cooper-Driver <jackson.cooper-driver@wadham.ox.ac.uk>
2021-04-23 19:20:36 +0000

    Updating timing txt name

--- linear_solver_lib/solver/L2/tests/gelinearsolver/IpVitisSolverInterface.cpp
+++ linear_solver_lib/solver/L2/tests/gelinearsolver/IpVitisSolverInterface.cpp
@@ -375,7 +375,7 @@ namespace Ipopt
           int trans2 = diff(&ttrans2,&tlaunch);
           int post =  diff(&tpost,&ttrans2);
           
-          FILE* fp = fopen("edit_getrf_while_multisolve.txt","a");
+          FILE* fp = fopen("edit_getrf_while_memset_multisolve.txt","a");
           
           if(multisolve_iteration == 1)
           {
